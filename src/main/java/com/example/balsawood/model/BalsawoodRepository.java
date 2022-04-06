90e780c93cb9ba96643fdf374c0fe3b66772e5a2
Juslin Jukka <jusju@haaga-helia.fi>
2022-04-06 14:21:22 +0300

    f

---
+++ src/main/java/com/example/balsawood/model/BalsawoodRepository.java
@@ -0,0 +1,7 @@
+package com.example.balsawood.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BalsawoodRepository extends CrudRepository<Balsawood, Long> {
+
+}
