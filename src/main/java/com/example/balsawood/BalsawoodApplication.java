b6bb7283d51a8979ec4e1e5cd3fe9258074988af
Juslin Jukka <jusju@haaga-helia.fi>
2022-04-06 13:26:14 +0300

    first commit

---
+++ src/main/java/com/example/balsawood/BalsawoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.balsawood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalsawoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalsawoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/balsawood/BalsawoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.balsawood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalsawoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
